2b18a9b2a3ae6022783a380a18b7a9cbc2cf4918
Mattia-Scarpa <scarpa.mat25@gmail.com>
2021-12-21 11:15:30 +0100

    initial commit

---
+++ build/CMakeFiles/calibration_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibration_main.dir/src/main.cpp.o"
+  "calibration_main"
+  "calibration_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibration_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
